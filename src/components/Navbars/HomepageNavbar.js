10b7361a4baf721d21753191e3cf28c08a2eb031
Jericho Gragasin <gragasinjericho26@gmail.com>
2022-06-05 02:43:09 +0800

    Created Landing Page Navbar

---
+++ src/components/Navbars/HomepageNavbar.js
@@ -0,0 +1,25 @@
+import {
+    Navbar,
+    Container,
+    Nav
+} from 'react-bootstrap';
+import '../../App.css';
+
+const HomepageNavbar = () => {
+    return (
+        <>
+            <Navbar bg="light" expand="lg" className="navbar">
+                <Container>
+                    <Navbar.Brand href="/" className="navbrand">Kick</Navbar.Brand>
+                    <Nav classname="justify-content-end">
+                        <Nav.Link className="navlink rounded-2" href="/">Home</Nav.Link>
+                        <Nav.Link className="navlink rounded-2" href="/login">Login</Nav.Link>
+                        <Nav.Link className="navlink rounded-2" href="/signup">Signup</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+
+export default HomepageNavbar;
