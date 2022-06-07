61d896670bc0b8812161faf0f79d7d2338bbba38
Jericho Gragasin <gragasinjericho26@gmail.com>
2022-06-08 02:10:53 +0800

    Added Form Validator

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,11 @@
-import React  from "react";
+import React, { useEffect }  from "react";
 import {
     useState,
 } from 'react';
 import {
     Button,
-    Form
+    Form, 
+    Alert
 } from 'react-bootstrap'
 
 
@@ -13,24 +14,45 @@ const Login = () => {
     const [password, setPassword] = useState("");
     const [isChecked, setIsChecked] = useState(false);
 
+    const initialState = {
+        emailErrMsg : "",
+        passwordErrMsg : ""
+    }
+
+    const [errMsg, setErrMsg] = useState(initialState);
+    // Parsing Object to make it accessible in the browser;
+    const errors = JSON.parse(JSON.stringify(errMsg));
+
+    const validateForm = (email, password) => {
+        if(email === ""){
+            errors.emailErrMsg = "Email Field is empty."
+        } else {
+            errors.emailErrMsg = ""
+        }
+        
+        if(password === ""){    
+            errors.passwordErrMsg = "Password Field is empty."
+        } else {
+            errors.passwordErrMsg = ""
+        }
+
+        return setErrMsg(errors);
+    }
+
     const loginHandler = (event) => {
         event.preventDefault();
-        console.log({
-            email: email,
-            password: password,
-            isChecked: isChecked
-        })
-
-        console.log("Submitted")
-        setEmail("");
-        setPassword("");
+        validateForm(email, password);
     }
+
     return (
         <Form className="w-50 mx-auto my-5 shadow-lg p-5 rounded-3 formContainer" onSubmit={loginHandler}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
+                {
+                    errMsg.emailErrMsg.length > 0 &&  <Alert variant="danger">{errMsg.emailErrMsg}</Alert>
+                }
                 <Form.Control 
-                    type="email" 
+                    type="email"     
                     placeholder="Enter email" 
                     autoComplete="new-password" 
                     value={email}
@@ -39,9 +61,11 @@ const Login = () => {
                 We'll never share your email with anyone else.
                 </Form.Text>
             </Form.Group>
-
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
+                {
+                    errMsg.passwordErrMsg.length > 0 &&  <Alert variant="danger">{errMsg.passwordErrMsg}</Alert>
+                }
                 <Form.Control 
                     type="password" 
                     placeholder="Password" 
